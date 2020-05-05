860e73f92cfc246d07cd491bf98c00c4df0f0f30
Geeoon Chung <geeoon13@gmail.com>
2020-05-05 09:25:51 -0500

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEn = new System.Windows.Forms.Button();
+            this.btnDis = new System.Windows.Forms.Button();
+            this.lblJoy = new System.Windows.Forms.Label();
+            this.lblEn = new System.Windows.Forms.Label();
+            this.btnShut = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.rtbLog = new System.Windows.Forms.RichTextBox();
+            this.lblSteer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnEn
+            // 
+            this.btnEn.Location = new System.Drawing.Point(12, 12);
+            this.btnEn.Name = "btnEn";
+            this.btnEn.Size = new System.Drawing.Size(63, 23);
+            this.btnEn.TabIndex = 0;
+            this.btnEn.Text = "Enable";
+            this.btnEn.UseVisualStyleBackColor = true;
+            this.btnEn.Click += new System.EventHandler(this.btnEn_Click);
+            // 
+            // btnDis
+            // 
+            this.btnDis.Location = new System.Drawing.Point(81, 12);
+            this.btnDis.Name = "btnDis";
+            this.btnDis.Size = new System.Drawing.Size(63, 23);
+            this.btnDis.TabIndex = 1;
+            this.btnDis.Text = "Disable";
+            this.btnDis.UseVisualStyleBackColor = true;
+            this.btnDis.Click += new System.EventHandler(this.btnDis_Click);
+            // 
+            // lblJoy
+            // 
+            this.lblJoy.AutoSize = true;
+            this.lblJoy.Location = new System.Drawing.Point(92, 75);
+            this.lblJoy.Name = "lblJoy";
+            this.lblJoy.Size = new System.Drawing.Size(0, 13);
+            this.lblJoy.TabIndex = 2;
+            this.lblJoy.Click += new System.EventHandler(this.lblJoy_Click);
+            // 
+            // lblEn
+            // 
+            this.lblEn.AutoSize = true;
+            this.lblEn.Location = new System.Drawing.Point(92, 52);
+            this.lblEn.Name = "lblEn";
+            this.lblEn.Size = new System.Drawing.Size(48, 13);
+            this.lblEn.TabIndex = 3;
+            this.lblEn.Text = "Disabled";
+            // 
+            // btnShut
+            // 
+            this.btnShut.Location = new System.Drawing.Point(150, 12);
+            this.btnShut.Name = "btnShut";
+            this.btnShut.Size = new System.Drawing.Size(81, 23);
+            this.btnShut.TabIndex = 4;
+            this.btnShut.Text = "Shut Down";
+            this.btnShut.UseVisualStyleBackColor = true;
+            this.btnShut.Click += new System.EventHandler(this.btnShut_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Current Status";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Sending Bytes: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(382, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Log: ";
+            // 
+            // rtbLog
+            // 
+            this.rtbLog.Location = new System.Drawing.Point(249, 33);
+            this.rtbLog.Name = "rtbLog";
+            this.rtbLog.ReadOnly = true;
+            this.rtbLog.Size = new System.Drawing.Size(300, 172);
+            this.rtbLog.TabIndex = 9;
+            this.rtbLog.Text = "";
+            // 
+            // lblSteer
+            // 
+            this.lblSteer.AutoSize = true;
+            this.lblSteer.Location = new System.Drawing.Point(93, 92);
+            this.lblSteer.Name = "lblSteer";
+            this.lblSteer.Size = new System.Drawing.Size(0, 13);
+            this.lblSteer.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(561, 217);
+            this.Controls.Add(this.lblSteer);
+            this.Controls.Add(this.rtbLog);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnShut);
+            this.Controls.Add(this.lblEn);
+            this.Controls.Add(this.lblJoy);
+            this.Controls.Add(this.btnDis);
+            this.Controls.Add(this.btnEn);
+            this.Name = "Form1";
+            this.Text = "Driver Station";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEn;
+        private System.Windows.Forms.Button btnDis;
+        private System.Windows.Forms.Label lblJoy;
+        private System.Windows.Forms.Label lblEn;
+        private System.Windows.Forms.Button btnShut;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox rtbLog;
+        private System.Windows.Forms.Label lblSteer;
+    }
+}
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SharpDX.XInput;
+
+namespace WindowsFormsApp1 //1k-2k
+{
+    public partial class Form1 : Form
+    {
+        private bool enabled = false;
+        Controller controller;
+        Gamepad gamepad;
+        public bool controllerConnected = false;
+        public int deadband = 2500;
+        public float leftThumbx, leftThumby, rightThumbx, rightThumby;
+        public float leftTrigger, rightTrigger;
+        sbyte lastRThy = 0;
+        sbyte lastLThx = 0;
+        byte[] data = Encoding.ASCII.GetBytes("");
+        string ipAddress = "192.168.0.42";
+        int sendPort = 1337;
+        public byte throttle;
+        public byte steer;
+        int everyFew = 0;
+        int everyFewT = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            controller = new Controller(UserIndex.One);
+            controllerConnected = controller.IsConnected;
+            Application.Idle += HandleApplicationIdle;
+        }
+
+        public void HandleApplicationIdle (object sender, EventArgs e)
+        {
+            while (IsApplicationIdle()) //continuous loop while idle; "game" loop
+            {
+                controllerConnected = controller.IsConnected;
+                if (controllerConnected == true)
+                {
+                    gamepad = controller.GetState().Gamepad;
+
+                    leftThumbx = (Math.Abs((float)gamepad.LeftThumbX) < deadband) ? 0 : (float)gamepad.LeftThumbX / short.MinValue * -100;
+                    leftThumby = (Math.Abs((float)gamepad.LeftThumbY) < deadband) ? 0 : (float)gamepad.LeftThumbY / short.MaxValue * 100;
+                    rightThumbx = (Math.Abs((float)gamepad.RightThumbX) < deadband) ? 0 : (float)gamepad.RightThumbX / short.MaxValue * 100;
+                    rightThumby = (Math.Abs((float)gamepad.RightThumbY) < deadband) ? 0 : (float)gamepad.RightThumbY / short.MaxValue * 100;
+
+                    leftTrigger = gamepad.LeftTrigger;
+                    rightTrigger = gamepad.RightTrigger;
+
+
+                    if (enabled == true)
+                    {
+
+
+                        lblEn.Text = "Enabled";
+                        throttle = (byte)rightThumby;
+                        steer = (byte)leftThumbx;
+
+                        
+                        if (lastRThy != (sbyte)rightThumby && everyFewT >= 1000)
+                        {
+                            everyFewT = 0;
+                            try
+                            {
+                                using (var client = new UdpClient())
+                                {
+                                    lblJoy.Text = throttle.ToString();
+                                    data = BitConverter.GetBytes(throttle);
+                                    IPEndPoint ep = new IPEndPoint(IPAddress.Parse(ipAddress), sendPort);
+                                    client.Connect(ep);
+                                    client.Send(data, data.Length);
+                                    client.Close();
+                                    lastRThy = (sbyte)rightThumby;
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+                                rtbLog.Text = rtbLog.Text + "\nError sending packet: " + ex.ToString();
+                            }
+                        }
+                        
+                        
+                        if (lastLThx != (sbyte)leftThumbx && everyFew >= 10000)
+                        {
+                            everyFew = 0;
+                            try
+                            {
+                                using (var client = new UdpClient())
+                                {
+                                    steer = (byte)(steer / (int)5);
+                                    if (steer <= 20)
+                                    {
+                                        steer = (byte)(steer + 30);
+                                    }
+                                    else
+                                    {
+                                        steer = (byte)((30 - (50 - steer)));
+                                    }
+                                    steer = (byte)(steer + 100);
+                                    lblSteer.Text = steer.ToString();
+                                    data = BitConverter.GetBytes(steer);
+                                    IPEndPoint ep = new IPEndPoint(IPAddress.Parse(ipAddress), sendPort);
+                                    client.Connect(ep);
+                                    client.Send(data, data.Length);
+                                    client.Close();
+                                    lastLThx = (sbyte)leftThumbx;
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+                                rtbLog.Text = rtbLog.Text + "\nError sending packet: " + ex.ToString();
+                            }
+                        }
+                        everyFewT++;
+                        everyFew++;
+                       
+
+                    }
+                    else
+                    {
+                        lblEn.Text = "Disabled";
+                        lblJoy.Text = "";
+                        lblSteer.Text = "";
+                        throttle = 0;
+                    }
+
+                    
+                } else {
+                    leftThumbx = 0f;
+                    leftThumby = 0f;
+                    rightThumbx = 0f;
+                    rightThumby = 0f;
+
+                    leftTrigger = 0f;
+                    rightTrigger = 0f;
+                }
+            }
+        }
+
+        private void btnEn_Click(object sender, EventArgs e)
+        {
+            enabled = true;
+            try
+            {
+                using (var client = new UdpClient())
+                {
+                    data = BitConverter.GetBytes(101);
+                    IPEndPoint ep = new IPEndPoint(IPAddress.Parse(ipAddress), sendPort);
+                    client.Connect(ep);
+                    client.Send(data, data.Length);
+                    client.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                rtbLog.Text = rtbLog.Text + "\nError sending packet: " + ex.ToString();
+            }
+        }
+
+        private void btnDis_Click(object sender, EventArgs e)
+        {
+            enabled = false;
+            try
+            {
+                using (var client = new UdpClient())
+                {
+                    data = BitConverter.GetBytes(102);
+                    IPEndPoint ep = new IPEndPoint(IPAddress.Parse(ipAddress), sendPort);
+                    client.Connect(ep);
+                    client.Send(data, data.Length);
+                    client.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                rtbLog.Text = rtbLog.Text + "\nError sending packet: " + ex.ToString();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnShut_Click(object sender, EventArgs e)
+        {
+            if (enabled == false)
+            {
+                try
+                {
+                    using (var client = new UdpClient())
+                    {
+                        data = BitConverter.GetBytes(105);
+                        IPEndPoint ep = new IPEndPoint(IPAddress.Parse(ipAddress), sendPort);
+                        client.Connect(ep);
+                        client.Send(data, data.Length);
+                        client.Close();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    rtbLog.Text = rtbLog.Text + "\nError sending packet: " + ex.ToString();
+                }
+            } else {
+                rtbLog.Text = rtbLog.Text + "\nYou must disable first before shutting down.";
+            }
+        }
+
+        private void lblJoy_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        bool IsApplicationIdle()
+        {
+            NativeMessage result;
+            return PeekMessage(out result, IntPtr.Zero, (uint)0, (uint)0, (uint)0) == 0;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct NativeMessage
+        {
+            public IntPtr Handle;
+            public uint Message;
+            public IntPtr WParameter;
+            public IntPtr LParameter;
+            public uint Time;
+            public Point Location;
+        }
+
+        [DllImport("user32.dll")]
+        public static extern int PeekMessage(out NativeMessage message, IntPtr window, uint filterMin, uint filterMax, uint remove);
+    }
+}
